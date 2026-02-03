3ecbe52f580bf5feeaddf9593de4606268213f4e
Isaac <isaacloff@gmail.com>
2026-02-03 16:08:52 +0000

    sealed stable address trait

--- src/registry/singular_registry/automated_registry/manual_registry/stable_address.rs
+++ src/registry/singular_registry/automated_registry/manual_registry/stable_address.rs
@@ -1,8 +1,14 @@
 // use std::rc::Rc;
 // use crate::prelude::sync::Arc;
 
-pub trait StableAddress {}
+pub trait StableAddress: sealed::Sealed {}
 
 impl<T> StableAddress for Box<T> {}
 // impl<T> StableAddress<T> for Arc<T> {}
 // impl<T> StableAddress<T> for Rc<T> {}
+
+mod sealed {
+    pub trait Sealed {}
+
+    impl<T> Sealed for Box<T> {}
+}
