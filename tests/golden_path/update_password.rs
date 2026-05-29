c26b425d00396dcda03531f973469583dfc26459
isaac <isaacloff@gmail.com>
2026-05-29 10:05:22 +0100

    Test: Can UpdatePassword

---
+++ tests/golden_path/update_password.rs
@@ -0,0 +1,30 @@
+use aion_state::{default::prelude::{Password, ReserverId}, prelude::{RegistryRegister, RegistryUnregister, RegistryUpdatePassword}};
+
+use crate::create_registry;
+
+#[test]
+pub fn can_update_password() {
+    let registry = create_registry();
+
+    let id = ReserverId::new("foo");
+    let password = Password::from(1);
+
+    let result = registry.register(RegistryRegister { id: id.clone(), password: password.clone() });
+
+    assert!(result.ok());
+
+    let result = registry.update_password(RegistryUpdatePassword {
+        id: &id,
+        old_password: &password,
+        new_password: Password::from(2)
+    });
+    
+    assert!(result.ok());
+
+    let result = registry.unregister(&RegistryUnregister {
+        id: &id,
+        password: &Password::from(2)
+    });
+
+    assert!(result.ok());
+}
