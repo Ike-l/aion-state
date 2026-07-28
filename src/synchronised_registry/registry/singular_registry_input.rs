d51f37c529199e3da698ec8cbc9eda7b15ccfff5
isaac <isaacloff@gmail.com>
2026-07-28 18:14:14 +0100

    renamed inputs from SingularRegistry -> Registry

--- src/synchronised_registry/registry/singular_registry_input.rs
+++ src/synchronised_registry/registry/singular_registry_input.rs
@@ -1,69 +1,69 @@
-pub struct SingularRegistryRegister<Id, Password> {
+pub struct RegistryRegister<Id, Password> {
     pub id: Id,
     pub password: Password
 }
 
-pub struct SingularRegistryUnregister<'a, Id, Password> {
+pub struct RegistryUnregister<'a, Id, Password> {
     pub id: &'a Id,
     pub password: &'a Password
 }
 
-pub struct SingularRegistryUpdatePassword<'a, Id, Password> {
+pub struct RegistryUpdatePassword<'a, Id, Password> {
     pub id: &'a Id,
     pub old_password: &'a Password,
     pub new_password: Password
 }
 
-pub struct SingularRegistryOwn<'a, Id, Password, ResourceId> {
+pub struct RegistryOwn<'a, Id, Password, ResourceId> {
     pub id: Id,
     pub password: &'a Password,
     pub resource_id: ResourceId
 }
 
-pub struct SingularRegistryReleaseResource<'a, Id, Password, ResourceId> {
+pub struct RegistryReleaseResource<'a, Id, Password, ResourceId> {
     pub id: &'a Id,
     pub password: &'a Password,
     pub resource_id: &'a ResourceId
 }
 
-pub struct SingularRegistryReleaseResourceAll<'a, Id, Password, ResourceId> {
+pub struct RegistryReleaseResourceAll<'a, Id, Password, ResourceId> {
     pub id: &'a Id,
     pub password: &'a Password,
     pub inputs: Vec<&'a ResourceId>
 }
 
-pub struct SingularRegistryAllow<'a, Id, Password, ResourceId, Access> {
+pub struct RegistryAllow<'a, Id, Password, ResourceId, Access> {
     pub id: &'a Id,
     pub password: &'a Password,
     pub resource_id: ResourceId,
     pub access: Access
 }
 
-pub struct SingularRegistryUnallow<'a, Id, Password, ResourceId, Access> {
+pub struct RegistryUnallow<'a, Id, Password, ResourceId, Access> {
     pub id: &'a Id,
     pub password: &'a Password,
     pub resource_id: &'a ResourceId,
     pub access: &'a Access
 }
 
-pub struct SingularRegistryCheckAccess<'a, Id, IdPassword, ResourceId, Access, Password> {
+pub struct RegistryCheckAccess<'a, Id, IdPassword, ResourceId, Access, Password> {
     pub user_details: Option<(&'a Id, &'a IdPassword)>,
     pub resource_id: &'a ResourceId,
     pub access: &'a Access,
     pub password: Option<&'a Password>
 }
 
-pub struct SingularRegistryReleaseAccess<'a, ResourceId, Access> {
+pub struct RegistryReleaseAccess<'a, ResourceId, Access> {
     pub resource_id: &'a ResourceId,
     pub access: &'a Access
 }
 
-pub struct SingularRegistryReleasingReleaseAccess<ResourceId, Access> {
+pub struct RegistryReleasingReleaseAccess<ResourceId, Access> {
     pub resource_id: ResourceId,
     pub access: Access
 }
 
-pub struct SingularRegistryReservation<'a, Id, IdPassword, ResourceId, Access, Password> {
+pub struct RegistryReservation<'a, Id, IdPassword, ResourceId, Access, Password> {
     pub id: Id,
     pub id_password: &'a IdPassword,
     pub resource_id: ResourceId,
@@ -71,33 +71,33 @@ pub struct SingularRegistryReservation<'a, Id, IdPassword, ResourceId, Access, P
     pub password: Option<&'a Password>
 }
 
-pub struct SingularRegistryUnreserve<'a, Id, Password, ResourceId, Access> {
+pub struct RegistryUnreserve<'a, Id, Password, ResourceId, Access> {
     pub id: &'a Id,
     pub password: &'a Password,
     pub resource_id: &'a ResourceId,
     pub access: &'a Access
 }
 
-pub struct SingularRegistryDrainReservations<'a, Id, Password> {
+pub struct RegistryDrainReservations<'a, Id, Password> {
     pub id: &'a Id,
     pub password: &'a Password
 }
 
-pub struct SingularRegistryAcquireAccess<'a, Id, IdPassword, ResourceId, Access, Password> {
+pub struct RegistryAcquireAccess<'a, Id, IdPassword, ResourceId, Access, Password> {
     pub user_details: Option<(&'a Id, &'a IdPassword)>,
     pub resource_id: ResourceId,
     pub access: Access,
     pub password: Option<&'a Password>
 }
 
-pub struct SingularRegistryReleasingAcquireAccess<Id, IdPassword, ResourceId, Access, Password> {
+pub struct RegistryReleasingAcquireAccess<Id, IdPassword, ResourceId, Access, Password> {
     pub user_details: Option<(Id, IdPassword)>,
     pub resource_id: ResourceId,
     pub access: Access,
     pub password: Option<Password>
 }
 
-pub struct SingularRegistrySaferReplacement<'a, Id, IdPassword, Access, ResourceId, Resource, Password> {
+pub struct RegistrySaferReplacement<'a, Id, IdPassword, Access, ResourceId, Resource, Password> {
     pub user_details: Option<(&'a Id, &'a IdPassword)>,
     pub access: &'a Access,
     pub resource_id: ResourceId,
@@ -105,6 +105,6 @@ pub struct SingularRegistrySaferReplacement<'a, Id, IdPassword, Access, Resource
     pub password: Option<&'a Password>,
 }
 
-pub struct SingularRegistryContainsResource<'a, ResourceId> {
+pub struct RegistryContainsResource<'a, ResourceId> {
     pub resource_id: &'a ResourceId
 }
