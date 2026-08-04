c690d99244e5b77a0de74f5edc3e537e237de357
isaac <isaacloff@gmail.com>
2026-08-04 19:31:30 +0100

    acquire nothing

---
+++ tests/acquire_access/acquire_nothing.rs
@@ -0,0 +1,38 @@
+use aion_state::prelude::{RegistryAcquireAccess, SynchronisedRegistryAcquireAccessError, RegistryReplacement};
+
+use crate::default::prelude::*;
+
+use crate::create_registry;
+
+#[test]
+fn can_acquire_nothing_t() {
+    can_acquire_nothing();
+}
+
+fn can_acquire_nothing() {
+    let registry = create_registry();
+
+    let resource_id = ResourceId::new_type::<String>();
+    let resource = Resource::new("resource".to_string());
+
+    let result = registry.checked_replace(RegistryReplacement {
+        user_details: None,
+        access: &Access::Replace,
+        resource_id: resource_id.clone(),
+        resource: Some(resource.clone()),
+        password: None,
+    });
+
+    assert!(result.ok());
+
+    let wrong_resource_id = ResourceId::new_type::<i32>();
+
+    let result = registry.acquire_access::<AccessResult<'_, Resource>>(RegistryAcquireAccess {
+        user_details: None,
+        resource_id: wrong_resource_id,
+        access: Access::Shared(1),
+        password: None
+    });
+
+    assert!(result.is_err_and(|err| err == SynchronisedRegistryAcquireAccessError::NotFound));
+}
--- tests/acquire_access/mod.rs
+++ tests/acquire_access/mod.rs
@@ -1,3 +1,4 @@
 mod acquire_one;
 mod acquire_many;
 mod acquire_conflicting;
+mod acquire_nothing;
