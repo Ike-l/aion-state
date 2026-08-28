c69a04345381c65388fe8f0c089d24b929e79d93
isaac <isaacloff@gmail.com>
2026-08-28 18:29:43 +0100

    refactored property manager

--- tests/brute/property_manager.rs
+++ tests/brute/property_manager.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
 
-use aion_state::prelude::{RegistryCheckOwner, RegistryContainsResource, RegistryIsOwned, RegistryOwn, RegistryRegister, RegistryReplacement};
+use aion_state::prelude::{RegistryCheckOwner, RegistryCheckedReplacementResult, RegistryContainsResource, RegistryIsOwned, RegistryOwn, RegistryRegister, RegistryReplacement};
 use tracing::{Level, event};
 
-use crate::{TestRegistry, brute::command::Command, default::prelude::Access};
+use crate::{TestRegistry, brute::command::Command, default::prelude::{Access, Password, ReserverId, Resource, ResourceId}};
 
 pub struct PropertyManager {}
 
@@ -15,134 +15,211 @@ impl PropertyManager {
     pub fn test(&self, registry: &Arc<TestRegistry>, command: Command) {
         event!(Level::INFO, "Executing Command: {command:?}");
         match command {
-            Command::CheckedReplacement{ user_details, access, resource_id, resource, password } => {
-                let resource_is_some = resource.is_some();
-                let registry_contains_resource = RegistryContainsResource { resource_id: &resource_id };
-                let resources = registry.len();
-                let contains = registry.contains_resource(&registry_contains_resource).ok();
+            Command::CheckedReplacement{ user_details, access, resource_id, resource, password } => self.test_checked_replacement(registry, user_details, access, resource_id, resource, password),
+            Command::Register { id, password } => self.test_register(registry, id, password),
+            Command::Own { id, password, resource_id } => self.test_own(registry, id, password, resource_id),
+        }
+    }
 
-                let registered = user_details.is_some_and(|(id, _)| registry.registered().contains(id));
+    fn test_checked_replacement(
+        &self,
+        registry: &Arc<TestRegistry>,
+        user_details: Option<(&ReserverId, &Password)>,
+        access: Access,
+        resource_id: ResourceId,
+        resource: Option<Resource>,
+        password: Option<Password>
+    ) {
+        let registry_contains_resource = RegistryContainsResource { resource_id: &resource_id };
+        let contains = registry.contains_resource(&registry_contains_resource).ok();
 
-                let input = RegistryReplacement {
-                    user_details,
-                    access: &access,
-                    resource_id: resource_id.clone(),
-                    resource,
-                    password: password.as_ref(),
-                };
-                let before_string = serde_json::to_string(registry.as_ref()).unwrap();
-                let result = registry.checked_replace(input.clone());
+        let resource_is_some = resource.is_some();
 
-                match access {
-                    Access::Replace => {
-                        // (change later when allow whitelist/blacklist)
-                        let is_owned = registry.is_owned(&RegistryIsOwned { resource_id: &resource_id });
-                        let owned_by_me = user_details.is_some_and(|(id, _)| registry.check_owner(&RegistryCheckOwner { id, resource_id: &resource_id }).ok());
+        let registered = user_details
+            .is_some_and(|(id, _)| 
+                registry.registered().contains(id)
+            );
 
-                        if is_owned {
-                            if !owned_by_me {
-                                assert!(!result.ok());
-                            } else {
-                                if !resource_is_some {
-                                    if contains {
-                                        assert_eq!(resources - 1, registry.len());
-                                    } else {
-                                        assert!(!result.ok());
-                                    }
-                                } else {
-                                    if !contains {
-                                        // can fail if len >= capacity (would reallocate)
-                                        // put test in later?
-                                        assert_eq!(resources + 1, registry.len(), "Result: {result}");
-                                    } else {
-                                        assert_eq!(resources, registry.len());
-                                    }
-                                    assert!(registry.contains_resource(&registry_contains_resource).ok());
-                                }
-                            }
-                        } else {
-                            if !resource_is_some {
-                                if !registered {   
-                                    if user_details.is_none() {
-                                        if !contains {
-                                            assert!(!result.ok());
-                                        } else {
-                                            assert!(result.ok());
-                                        }
-                                    } else {
-                                        assert!(!result.ok());
-                                    }
-                                } else {
-                                    if contains {
-                                        assert_eq!(resources - 1, registry.len());
-                                    } else {
-                                        assert!(!result.ok());
-                                    }
-                                }
-                            } else {
-                                if !contains {
-                                    if !registered {
-                                        if user_details.is_none() {
-                                            assert!(result.ok());
-                                        } else {
-                                            assert!(!result.ok());
-                                        }
-                                    } else {
-                                        // can fail if len >= capacity (would reallocate)
-                                        // put test in later?
-                                        assert_eq!(resources + 1, registry.len(), "Result: {result}");
-                                    }
-                                } else {
-                                    assert_eq!(resources, registry.len());
-                                }
+        let resources_before = registry.len();
 
-                                if !contains && !registered {
-                                    // todo!();
-                                } else {
-                                    assert!(registry.contains_resource(&registry_contains_resource).ok());
-                                }
-                            }
-                        }
-                    },
-                    _ => {
-                        assert!(!result.ok())
-                    },
-                }
-            },
-            Command::Register { id, password } => {
-                let already_registered = registry.registered().contains(&id);
-                let result = registry.register(RegistryRegister {
-                    id: id.clone(),
-                    password,
-                });
+        let registry_is_owned = RegistryIsOwned { resource_id: &resource_id };
+        let owned = registry.is_owned(&registry_is_owned);
 
-                assert!(registry.registered().contains(&id));
-                assert_eq!(!already_registered, result.ok());
-            },
-            Command::Own { id, password, resource_id } => {
-                let registered = registry.registered().contains(&id);
-                let input = RegistryIsOwned {
-                    resource_id: &resource_id
-                };
+        let owned_by_user = user_details
+            .as_ref()
+            .is_some_and(|(id, _password)|
+                registry.check_owner(&RegistryCheckOwner { id, resource_id: &resource_id }).ok() 
+            );
 
-                let is_owned = registry.is_owned(&input);
-                let result = registry.own(RegistryOwn {
-                    id,
-                    password: &password,
-                    resource_id: resource_id.clone(),
-                });
+        let input = RegistryReplacement {
+            user_details,
+            access: &access,
+            resource_id: resource_id.clone(),
+            resource,
+            password: password.as_ref(),
+        };
 
-                if !registered {
-                    assert!(!result.ok());
-                } else {
-                    if is_owned {
-                        assert!(!result.ok());
-                    } else {
-                        assert!(registry.is_owned(&input));
-                        assert!(result.ok());
-                    }
-                }
+        let result = registry.checked_replace(input.clone());
 
+        if !matches!(access, Access::Replace) {
+            assert!(!result.ok());
+            return;
+        }
+
+        self.assert_checked_replacement_result(
+            registry, 
+            result, 
+            contains, 
+            resource_is_some, 
+            registered, 
+            user_details.is_some(), 
+            owned, 
+            owned_by_user, 
+            resources_before, 
+            &registry_contains_resource
+        );
+    }
+
+    fn assert_checked_replacement_result(
+        &self,
+        registry: &Arc<TestRegistry>,
+        result: RegistryCheckedReplacementResult<Resource>,
+        contains: bool,
+        resource_is_some: bool,
+        registered: bool,
+        has_user_details: bool,
+        owned: bool,
+        owned_by_user: bool,
+        resources_before: usize,
+        registry_contains_resource: &RegistryContainsResource<'_, ResourceId> 
+    ) {
+        if owned {
+            if !owned_by_user {
+                assert!(!result.ok());
+                return
+            }
+
+            self.assert_checked_replacement_mutation(
+                registry,
+                result,
+                contains,
+                resource_is_some,
+                resources_before,
+                registry_contains_resource
+            );
+
+            return
+        }
+
+        if !registered {
+            if has_user_details {
+                assert!(!result.ok());
+                return
+            }
+
+            if !resource_is_some && contains {
+                assert!(result.ok());
+                return
+            }
+
+            if !resource_is_some {
+                assert!(!result.ok());
+                return
+            }
+
+            if !contains {
+                assert!(result.ok());
+                return
+            }
+
+            assert_eq!(resources_before, registry.len());
+            assert!(registry.contains_resource(registry_contains_resource).ok());
+            return
+        }
+
+        self.assert_checked_replacement_mutation(
+            registry,
+            result,
+            contains,
+            resource_is_some,
+            resources_before,
+            registry_contains_resource
+        );
+    }
+
+    fn assert_checked_replacement_mutation(
+        &self,
+        registry: &Arc<TestRegistry>,
+        result: RegistryCheckedReplacementResult<Resource>,
+        contains: bool,
+        resource_is_some: bool,
+        resources_before: usize,
+        registry_contains_resource: &RegistryContainsResource<'_, ResourceId> 
+    ) {
+        if !resource_is_some {
+            if contains {
+                assert_eq!(resources_before - 1, registry.len());
+            } else {
+                assert!(!result.ok());
+            }
+
+            return
+        }
+
+        if contains {
+            assert_eq!(resources_before, registry.len());
+        } else {
+            // This can fail if len >= capacity and replacement reallocates
+            assert_eq!(resources_before + 1, registry.len());
+        }
+
+        assert!(registry.contains_resource(registry_contains_resource).ok())
+    }
+
+    fn test_register(
+        &self,
+        registry: &Arc<TestRegistry>,
+        id: ReserverId,
+        password: Password
+    ) {
+        let already_registered = registry.registered().contains(&id);
+        
+        let result = registry.register(RegistryRegister {
+            id: id.clone(),
+            password,
+        });
+
+        assert!(registry.registered().contains(&id));
+        assert_eq!(!already_registered, result.ok());
+    }
+
+    fn test_own(
+        &self,
+        registry: &Arc<TestRegistry>,
+        id: ReserverId,
+        password: Password,
+        resource_id: ResourceId
+    ) {
+        let registered = registry.registered().contains(&id);
+
+        let input = RegistryIsOwned {
+            resource_id: &resource_id
+        };
+
+        let already_owned = registry.is_owned(&input);
+
+        let result = registry.own(RegistryOwn {
+            id,
+            password: &password,
+            resource_id: resource_id.clone(),
+        });
+
+        match (registered, already_owned) {
+            (false, _) => assert!(!result.ok()),
+            (true, true) => assert!(!result.ok()),
+            (true, false) => {
+                assert!(result.ok());
+                assert!(registry.is_owned(&input))
             }
         }
     }
